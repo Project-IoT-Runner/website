214a714c43c3a14f00e5e86f5921e50186f5a6f9
Bauke Westendorp <baukewestendorp@gmail.com>
2025-03-06 18:34:59 +0100

    +layout.page versimpelen
    Ik denk dat het beter is om de header en zo niet een Resizable te maken omdat het raar is om dat te kunnen verslepen, maar we kunnen dat wel gebruiken voor een sidebar of zo.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	import * as Resizable from "$lib/components/ui/resizable/";
-	let { children } = $props();
+    import '../app.css';
+
+    let { children } = $props();
 </script>
 
-
-<Resizable.PaneGroup
-  direction="vertical"
-  class="min-h-screen max-w-full"
->
-  <Resizable.Pane defaultSize={10}>
-    <div class="flex h-full bg-emerald-950 p-6">
-      <span class="font-semibold text-white">Header</span>
-    </div>
-  </Resizable.Pane>
-  <Resizable.Handle />
-  <Resizable.Pane defaultSize={90}>
-    <div class="flex h-full p-6">
-      <span class="font-semibold">{@render children()}</span>
-    </div>
-  </Resizable.Pane>
-</Resizable.PaneGroup>
-  
-
+<header class="border-b bg-gray-100 px-8 py-4">
+    <h1 class="text-2xl font-bold">
+        <a href="/" class="no-underline hover:underline">Runner</a>
+    </h1>
+</header>
+<main class="mx-auto my-8 max-w-5xl">
+    {@render children()}
+</main>
+<footer></footer>
