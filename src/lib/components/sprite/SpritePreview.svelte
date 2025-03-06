3f00ae6e5ea21aa64c7ba503a75da3f4a251a778
Bauke Westendorp <baukewestendorp@gmail.com>
2025-03-06 23:24:30 +0100

    `SpritePreview` daadwerkelijk vierkant maken :^)

--- src/lib/components/sprite/SpritePreview.svelte
+++ src/lib/components/sprite/SpritePreview.svelte
@@ -4,7 +4,7 @@
     let { pixelData }: { pixelData: PixelData } = $props();
 </script>
 
-<div class="grid aspect-square h-full w-full grid-cols-8 grid-rows-8">
+<div class="grid aspect-square w-full grid-cols-8 grid-rows-8">
     {#each pixelData as pixel, ix (ix)}
         <div class:bg-primary={pixel} class="h-full w-full"></div>
     {/each}
