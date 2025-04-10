d4dd75c8d40068c7249bfe35a887aa1ca4047ee0
Bauke Westendorp <baukewestendorp@gmail.com>
2025-04-10 11:53:09 +0200

    Verbeter sprite view pagina

--- src/routes/sprite/[id]/+page.svelte
+++ src/routes/sprite/[id]/+page.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
-    import { page } from '$app/state';
     import SpritePreview from '$lib/components/sprite/SpritePreview.svelte';
-	import type { PageProps } from './$types';
+    import { Button } from '$lib/components/ui/button';
+    import type { PageProps } from './$types';
 
-	let { data }: PageProps = $props();
-	let { sprite } = data;
+    let { data }: PageProps = $props();
+    let { sprite } = data;
 </script>
 
-<div class="w-full h-full flex justify-center  ">
-	<div class="w-96">
-		<SpritePreview pixelData={sprite.pixels} />
-	</div>
-</div>
+<div class="flex h-full w-full flex-col items-center gap-20">
+    <div class="flex w-full justify-between">
+        <h2 class="text-3xl font-bold">{sprite.name}</h2>
+        <Button variant="link" href="/sprite/{sprite.id}/edit">Bewerk</Button>
+    </div>
 
+    <div class="w-96">
+        <div class="aspect-square w-full rounded-md border bg-secondary p-2">
+            <SpritePreview pixelData={sprite.pixels} />
+        </div>
+    </div>
+</div>
